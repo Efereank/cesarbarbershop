b97bcb9f373d7b3ba6c133ae8a282dce6dcc24d8
frank <franciscojavier20006@gmail.com>
2026-01-24 21:32:37 -0400

    cambiofin

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,30 +1,36 @@
-<h1 class="nombre-pagina">Cesar's BarbershSSop</h1>
+<h1 class="nombre-pagina">Cesar's Barbershop</h1>
 <p class="descripcion-pagina-login">Iniciar Sesión</p>
 
-<?php
-        include_once __DIR__ . "/../templates/alertas.php";
-?>
+<?php include_once __DIR__ . "/../templates/alertas.php"; ?>
 
 <form class="formulario" method="POST" action="/">
+  
+  <div class="campo-login">
+    <input 
+      type="email" 
+      id="correo" 
+      placeholder="Tu Correo electrónico" 
+      name="correo" 
+      value="<?php echo s($auth->correo); ?>"
+      required
+    >
+  </div>
 
-<div class="campo-login">
-        <label for="email"></label>
-        <input type="email" id="correo" placeholder="Tu Correo electrónico" name="correo" value="<?php echo s($auth->correo); ?>">
-</div>
+  <div class="campo-login">
+    <input 
+      type="password" 
+      id="password" 
+      placeholder="Tu Contraseña" 
+      name="password"
+      required
+    >
+  </div>
 
-
-
-<div class="campo-login">
-        <label for="password"></label>
-        <input type="password" id="password" placeholder="Tu Contraseña" name="password">
-</div>
-
-
-<input class="boton" type="submit" value="Entrar">
+  <input class="boton" type="submit" value="Entrar">
 
 </form>
 
 <div class="acciones">
-    <a href="/sign-up">¿Aún no tienes una cuenta? Crear una.</a>
-    <a href="/recuperar-cuenta">¿Olvidaste tu Contraseña?</a>
+  <a href="/sign-up">¿Aún no tienes una cuenta? Crear una.</a>
+  <a href="/recuperar-cuenta">¿Olvidaste tu Contraseña?</a>
 </div>
